8d3b217971b2538b25ed3589df4d93c279e07da7
Rachel Brynsvold <rachel.brynsvold@gmail.com>
2017-09-28 00:03:01 -0500

    dim red script ready for testing on nlp research box

--- py_scripts/dim_reduction.py
+++ py_scripts/dim_reduction.py
@@ -38,18 +38,28 @@ class IterFile(object):
         self._close_file()
 
 
-def create_save_objs(source_dir, outputs_dir, distinguishing_str, stop_words=1, min_freq=1):
+def create_save_objs(source_dir, outputs_dir, distinguishing_str, stop_words='Y', min_freq=1):
     '''
     Call all subfunctions.
     Create and save gensim objects needed for lda model (corpus, dictionary).
     '''
     fileid_lst = get_fileid_lst(source_dir)
+
     all_transf_books_lst = [transform_txt_file(f, source_dir, stop_words, min_freq=None) for f in fileid_lst]
+
     dictionary = corpora.Dictionary(all_transf_books_lst)
     corpus = [dictionary.doc2bow(book) for book in all_transf_books_lst]
+
     avg_num_tokens, avg_unique_toks, dictionary_length, toks_per_fileid, unique_toks_per_fileid = dim_red_counts(fileid_lst, all_transf_books_lst, dictionary, corpus)
 
     save_stuff(distinguishing_str, dictionary, corpus, outputs_dir)
+
+    print "Dimensional reduction complete!"
+    print "After dimensional reduction:"
+    print "   "
+    print "Average total tokens per book:        ", avg_num_tokens
+    print "Average unique tokens per book:       ", avg_unique_toks
+    print "Number of unique words in vocabulary: ", dictionary_length
 
     return avg_num_tokens, avg_unique_toks, dictionary_length, toks_per_fileid, unique_toks_per_fileid
     #need to write dimensionality reduction stuff to file for review
@@ -75,7 +85,7 @@ def transform_txt_file(fname, root, stop_words, min_freq=1):
     if min_freq != None:
         book_as_lst = frequency_filtering(book_as_lst, n=min_freq)
 
-    print "another book complete"
+    print fname, " transformed"
     return book_as_lst
 
 
@@ -91,7 +101,7 @@ def dim_red_counts(fileid_lst, all_transf_books_lst, dictionary, corpus):
     #could actually do this better by redoing like it was done for the unique_toks stuff
 
     avg_num_tokens = int(np.mean([pair[1] for pair in toks_per_fileid]))
-    #changing this to avoid going thru full text again
+    #changed this from what I had in jn to avoid going thru full text again
     #avg_num_tokens = int(np.mean([len(book) for book in all_transf_books_lst]))
 
     unique_toks_num_lst = [len(book) for book in corpus]
@@ -105,13 +115,18 @@ def dim_red_counts(fileid_lst, all_transf_books_lst, dictionary, corpus):
 
 def save_stuff(distinguishing_str, dictionary, corpus, outputs_dir):
     '''
-    Save the outputs of the desired object(s)
+    Create directory and save the outputs of the desired object(s)
     '''
+    file_path = outputs_dir + distinguishing_str
+    directory = os.path.dirname(file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
     if dictionary != None:
-        dictionary.save(outputs_dir + distinguishing_str + '.dict')
+        dictionary.save(file_path + '.dict')
 
     if corpus != None:
-        corpora.MmCorpus.serialize(outputs_dir + distinguishing_str + '_corpus.mm', corpus)
+        corpora.MmCorpus.serialize(file_path + '_corpus.mm', corpus)
 
 
 def empty_line_check(line) :
@@ -137,12 +152,13 @@ def remove_stop_words(line):
     '''
     Get the stopwords list from nltk and take them out of the line
     '''
-
     stop = set(stopwords.words('english'))
 
     return [tok for tok in line if tok not in stop]
 
-##THIS GOES TOO SLOW
+
+##THIS GOES TOO SLOW -
+#to add the freq filtering functionality back in (if time permits), need to do it like here: https://stackoverflow.com/questions/24688116/how-to-filter-out-words-with-low-tf-idf-in-a-corpus-with-gensim
 def frequency_filtering(book_as_lst, n):
     '''
     Remove words that appear less than n times
@@ -157,21 +173,25 @@ def get_fileid_lst(source_dir):
     '''
     Use NLTK to pull in the list of file ids in the given source directory
     '''
-
     temp_corp = PlaintextCorpusReader(source_dir, '.*\.txt')
     fileid_lst = temp_corp.fileids()
+
     return fileid_lst
 
 
 if __name__=='__main__':
-    #set source and output directories (with user prompting??)
-    #source_dir = '/home/ubuntu/data_download/clean_books/'
-    #outputs_dir = '/home/ubuntu/Capstone/outputs/full_data/'
 
-    source_dir = '/Users/rachelbrynsvold/dsi/capstone_dir/Capstone/books/clean' + '/'
-    outputs_dir = '/Users/rachelbrynsvold/dsi/capstone_dir/Capstone/outputs/junk' + '/'
-    distinguishing_str='first_test'
+    distinguishing_str = str(raw_input("Enter brief identifier string, to be appended to all outputs of this dimensional reduction: "))
+
+    #relative filepaths
+    source_dir  = '../books/clean' + '/' #for 95-book practice data
+    #source_dir = '../../clean_books' + '/'  #for full data set
+    outputs_dir = '../outputs' + '/' # same both ways
 
     create_save_objs(source_dir, outputs_dir, distinguishing_str)
-    #run most complete version of dim red by defaultdict
-    #user can call intermediate dim red modules if needed
+
+    ##Backup: hardcoded dir options
+    #source_dir = '/home/ubuntu/data_download/clean_books/'
+    #outputs_dir = '/home/ubuntu/Capstone/outputs/full_data/'
+    #source_dir = '/Users/rachelbrynsvold/dsi/capstone_dir/Capstone/books/clean' + '/'
+    #outputs_dir = '/Users/rachelbrynsvold/dsi/capstone_dir/Capstone/outputs/junk' + '/'
