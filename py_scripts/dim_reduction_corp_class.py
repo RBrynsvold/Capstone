e27e0332352fcb62725f4e58cd6d55e0694ce7ad
Rachel Brynsvold <rachel.brynsvold@gmail.com>
2017-09-30 02:17:42 -0500

    splitting into multiple dictionaries

--- py_scripts/dim_reduction_corp_class.py
+++ py_scripts/dim_reduction_corp_class.py
@@ -95,17 +95,17 @@ def create_save_objs(source_dir, outputs_dir, distinguishing_str, stop_words='Y'
     Create and save gensim objects needed for lda model (corpus, dictionary).
     '''
     fileid_lst = get_fileid_lst(source_dir)
-    books_lst_filep = '../..' + distinguishing_str + '_lst.txt'
+    books_lst_filep = '../' + distinguishing_str + '_lst.txt'
 
     print "starting iteration thru corpus"
     print "************************************************"
 
-    dictionary, books_lst_filep = process_books(fileid_lst, books_lst_filep)
+    dictionary, books_lst_filep = process_books(fileid_lst, books_lst_filep, outputs_dir)
 
     print "transformations and dictionary building complete"
     print "************************************************"
 
-    corpus = create_corp(dictionary, books_lst_filep, source_dir)
+    corpus = create_corp(dictionary, books_lst_filep, outputs_dir)
 
     print "corpus complete"
     print "************************************************"
@@ -124,39 +124,75 @@ def create_save_objs(source_dir, outputs_dir, distinguishing_str, stop_words='Y'
     # return avg_num_tokens, avg_unique_toks, dictionary_length, toks_per_fileid, unique_toks_per_fileid
     #need to write dimensionality reduction stuff to file for review
 
-def process_books(fileid_lst, books_lst_filep):
+def process_books(fileid_lst, books_lst_filep, outputs_dir):
     '''
     Iterate thru all books and create a dictionary.
     Save each 'book-as-list' to a file, to use later to create the corpus.
     '''
-    dictionary = corpora.dictionary.Dictionary()
-    stop = set(stopwords.words('english'))
-
     f = codecs.open(books_lst_filep, 'w', encoding='utf_8')
+    stop = set(stopwords.words('english'))
+    onek_books_lst, dicts_count = [], 0
+    dicts_fp = outputs_dir + 'tmp_dicts/'
 
-    for f_id in fileid_lst:
+    for num, f_id in enumerate(fileid_lst):
+        adj_num = num + 1
 
         current_book = BookUtil(f_id, source_dir, stop)
         current_book.transform()
 
         current_book_lst = current_book.book_as_lst
+        tmp_book_lst = [val for sublist in current_book_lst for val in sublist]
 
-        # dictionary.add_documents(current_book_lst)
+        onek_books_lst.append(tmp_book_lst)
 
-        one_d_book_lsts = [val for sublist in current_book_lst for val in sublist]
+        if adj_num % 1000 == 0:
+            create_save_dicts(onek_books_lst, dicts_fp, dicts_count, final_merge='n')
+            onek_books_lst = []
+            dicts_count += 1
 
-        f.write(u",".join(one_d_book_lsts) + '\n')
+        f.write(u",".join(tmp_book_lst) + '\n')
+
+    dictionary = create_save_dicts(onek_books_lst, dicts_fp, dicts_count, final_merge="y")
+
+    print "returned dictionary is this size: ", len(dictionary)
+
     f.close()
 
+    print "returning the books list file path " + books_lst_filep
+
     return dictionary, books_lst_filep
+
+
+def create_save_dicts(tmp_books_lst, dicts_fp, dicts_count, final_merge="y"):
+    '''
+    Save out intermediate dictionaries.
+    When all the files have been processed, save the last piece and merge all the dictionaries.
+    '''
+    d_tmp = corpora.dictionary.Dictionary()
+    d_tmp.add_documents(tmp_books_lst)
+    d_tmp.save(dicts_fp + str(dicts_count) + '.dict')
+
+    print "added ", len(d_tmp), " to temp dictionary"
+
+    if final_merge == 'y':
+        final_dict = d_tmp #corpora.dictionary.Dictionary()
+        for n in range(dicts_count):
+            loaded_dict = corpora.dictionary.Dictionary.load(dicts_fp + str(n) + '.dict')
+            final_dict.merge_with(loaded_dict)
+            print "dictionary", n, "loaded & merged"
+        return final_dict
 
 def create_corp(dictionary, books_lst_filep, source_dir):
     '''
     Open big file of 'books-as-lists', stream it in, and use to create corpus.
     '''
-    f = IterFile(books_lst_filep, outputs_dir)
+    f = IterFile(books_lst_filep, '')
 
-    corpus = [dictionary.doc2bow(book.strip('/n').split()) for book in f]
+    #print "the file ", f.file.name
+
+    corpus = [dictionary.doc2bow(book.strip('/n').split(',')) for book in f]
+
+    return corpus
 
 def dim_red_counts(fileid_lst, all_transf_books_lst, dictionary, corpus):
     '''
@@ -194,9 +230,13 @@ def save_stuff(distinguishing_str, dictionary, corpus, outputs_dir):
 
     if dictionary != None:
         dictionary.save(file_path + '.dict')
+    else:
+        print "no dictionary to save"
 
     if corpus != None:
         corpora.MmCorpus.serialize(file_path + '_corpus.mm', corpus)
+    else:
+        print "No corpus to save"
 
 
 ##THIS GOES TOO SLOW -
