476262fcb5c9fc0b5c86d5d54260fe5a51565ac9
Rachel Brynsvold <rachel.brynsvold@gmail.com>
2017-09-30 12:48:12 -0500

    rsync bash script fix

--- bash_scripts/rsync_cleaned_data_from_mirror.sh
+++ bash_scripts/rsync_cleaned_data_from_mirror.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -azv -e "ssh -i ~/aws.pem" vagrant@ec2-54-187-164-38.us-west-2.compute.amazonaws.com:/home/vagrant/ /home/vagrant/data/clean_books
+rsync -azv -e "ssh -i ~/aws.pem" vagrant@ec2-54-187-164-38.us-west-2.compute.amazonaws.com:/home/vagrant/data/clean_books /home/vagrant/clean_books
