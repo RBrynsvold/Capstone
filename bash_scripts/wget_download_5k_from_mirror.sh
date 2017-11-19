b0c27f83b084eb8c454ff26bcd432178c90af7d8
Rachel Brynsvold <rachel.brynsvold@gmail.com>
2017-11-19 10:06:44 -0600

    manual upload

---
+++ bash_scripts/wget_download_5k_from_mirror.sh
@@ -0,0 +1,1 @@
+wget -r -nd --no-parent http://54.187.164.38:5000/5000_books -P /home/vagrant/5000_books
---
+++ bash_scripts/wget_download_full_corp_from_mirror.sh
@@ -0,0 +1,1 @@
+wget -r -nd --no-parent http://54.187.164.38:5000/full_corp_28k/clean_books -P full_corp_28k
